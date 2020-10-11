4ea679963cf1cc2eb3729243dc831cdddd2ab209
sralloza <sralloza@gmail.com>
2020-10-11 12:23:24 +0200

    feat: automatically reload server

--- start-api.sh
+++ start-api.sh
@@ -1,1 +1,1 @@
-gunicorn -k uvicorn.workers.UvicornWorker -w 2 -b :8000 app:app  --log-level DEBUG --access-logfile "/srv/logs/api/api.log" --error-logfile "/srv/logs/api/error.log" 2>/srv/logs/api/output.log &
+gunicorn -k uvicorn.workers.UvicornWorker -w 2 -b :8000 app:app --reload --log-level DEBUG --access-logfile "/srv/logs/api/api.log" --error-logfile "/srv/logs/api/error.log" 2>/srv/logs/api/output.log &
