4ffe07861bcbc3f400e715d703c328a8fb4da999
sralloza <sralloza@gmail.com>
2020-10-11 12:13:00 +0200

    feat: implement OAuth

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 
 from . import bot, users
 
@@ -6,5 +6,5 @@ app = FastAPI(
     title="Health Bot API", description="Backend for Health Bot", version="1.0.0a1"
 )
 
-app.include_router(bot.router)
+app.include_router(bot.router, dependencies=[Depends(users.get_current_user)])
 app.include_router(users.router)
--- app/users.py
+++ app/users.py
@@ -1,25 +1,130 @@
+from datetime import datetime, timedelta
+from typing import Optional
+
+from fastapi import APIRouter, Depends, status
 from fastapi.exceptions import HTTPException
-from fastapi.routing import APIRouter
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from passlib.context import CryptContext
 from pydantic.main import BaseModel
 
 router = APIRouter()
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+SECRET_KEY = "bc1994fc98b4757f41cadf698c28bb06a6a560f59f74c27c9e81e71304f78010"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
 
 class User(BaseModel):
     username: str
-    password: str
 
 
-valid_users = [("mark", "asdf"), ("user", "password")]
-valid_users = [User(username=a, password=b) for (a, b) in valid_users]
+class DBUser(User):
+    hashed_password: str
 
 
-@router.post("/login")
-def login_endpoint(user: User):
+valid_users = [
+    ("patrice", "patrice"),
+    ("username", "password"),
+    ("user", "pass"),
+    ("diego", "diego"),
+    ("pablo", "pablo"),
+]
+
+valid_users = [
+    DBUser(username=a, hashed_password=get_password_hash(b)) for (a, b) in valid_users
+]
+
+
+def get_user(username: str):
+    for user in valid_users:
+        if username == user.username:
+            return user
+
+
+def authenticate_user(username: str, hashed_password: str):
+    user = get_user(username)
+    if not user:
+        return False
+    if not verify_password(hashed_password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+@router.post("/login", response_model=Token)
+def login_endpoint(form_data: OAuth2PasswordRequestForm = Depends()):
     """Login endpoint."""
-    if user not in valid_users:
-        raise HTTPException(status_code=401, detail="Invalid user")
-    return {"status": "ok"}
+    user = authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@router.get("/users/me")
+async def read_users_me(current_user: User = Depends(get_current_user)):
+    return current_user
 
 
 @router.post("/logout")
