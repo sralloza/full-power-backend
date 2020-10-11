c2f05b863ffb4e4e4e506b030863e459447d8b51
sralloza <sralloza@gmail.com>
2020-10-11 13:53:01 +0200

    feat: allow all origins (disable CORS)

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from . import bot, users
 
@@ -8,3 +9,16 @@ app = FastAPI(
 
 app.include_router(bot.router, dependencies=[Depends(users.get_current_user)])
 app.include_router(users.router)
+
+origins = [
+    "https?://localhost:*",
+    "https?://api.sralloza.es",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
