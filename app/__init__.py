7fdb6d58f7d3621f071fd88222c521f7e3bbc361
sralloza <sralloza@gmail.com>
2020-10-11 13:15:49 +0200

    feat: add dotenv

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
+from dotenv import load_dotenv
 from fastapi import Depends, FastAPI
 
 from . import bot, users
 
+load_dotenv()
 app = FastAPI(
     title="Health Bot API", description="Backend for Health Bot", version="1.0.0a1"
 )
