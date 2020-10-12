b65ba74fbd250bcbce501ef311af363d7f35cc82
sralloza <sralloza@gmail.com>
2020-10-12 13:53:07 +0200

    feat: disable docs on production

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,24 @@
+import os
+
 from dotenv import load_dotenv
 from fastapi import Depends, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.database import engine, models
-
-from . import bot, security, users, conversations
+from . import bot, conversations, security, users
+from .database import engine, models
 
 models.Base.metadata.create_all(bind=engine)
 
 load_dotenv()
-app = FastAPI(
+
+fastapi_kwargs = dict(
     title="Health Bot API", description="Backend for Health Bot", version="1.0.0a1"
 )
+
+if os.getenv("PRODUCTION", False):
+    fastapi_kwargs.update(docs_url=None, redoc_url=None)
+
+app = FastAPI(**fastapi_kwargs)
 
 
 app.include_router(security.router, tags=["security"])
