b65d0fcf4f9cd9a837398a38dd32a9b90be593c3
sralloza <sralloza@gmail.com>
2020-10-12 11:15:27 +0200

    feat: add /users/me endpoint

--- app/security/__init__.py
+++ app/security/__init__.py
@@ -60,7 +60,7 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     return encoded_jwt
 
 
-async def get_current_user(token: str = Depends(oauth2_scheme)):
+def get_current_user(token: str = Depends(oauth2_scheme)):
     from app.users.crud import get_user_by_username
 
     credentials_exception = HTTPException(
--- app/users/__init__.py
+++ app/users/__init__.py
@@ -1,6 +1,7 @@
 from typing import List
 
-from fastapi import APIRouter, HTTPException
+from app.security import get_current_user
+from fastapi import APIRouter, Depends, HTTPException
 
 from . import crud, schemas
 
@@ -47,3 +48,8 @@ def users_get_one(user_id: int):
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
+
+
+@router.get("/users/me/", response_model=schemas.User)
+def users_get_current_user(current_user: schemas.User = Depends(get_current_user)):
+    return current_user
