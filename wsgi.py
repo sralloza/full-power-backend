0c89249b285a1c54be33fa99ad4032268c2a2074
sralloza <sralloza@gmail.com>
2020-10-16 22:17:43 +0200

    fix: import a2wsgi after loading virtualenv

--- wsgi.py
+++ wsgi.py
@@ -2,12 +2,6 @@ import sys
 from pathlib import Path
 from platform import system
 
-try:
-    from a2wsgi import ASGIMiddleware
-except ImportError:
-    print("Install a2wsgi with 'pip install a2wsgi'", sys.stderr)
-    sys.exit(1)
-
 if system() == "Windows":
     activate_this_path = ".venv/scripts/activate_this.py"
 else:
@@ -17,6 +11,12 @@ activate_this_path = Path(__file__).parent.joinpath(activate_this_path)
 with activate_this_path.open() as file:
     exec(file.read(), dict(__file__=activate_this_path.as_posix()))
 
+
+try:
+    from a2wsgi import ASGIMiddleware
+except ImportError:
+    print("Install a2wsgi with 'pip install a2wsgi'", sys.stderr)
+    sys.exit(1)
 
 sys.path.insert(0, Path(__file__).parent.as_posix())
 from app import app
