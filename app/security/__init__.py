21d0557152c491cd6d299ab69685f3998d42440b
sralloza <sralloza@gmail.com>
2020-10-20 11:09:59 +0200

    feat: add settings endpoint for admins

--- app/security/__init__.py
+++ app/security/__init__.py
@@ -3,13 +3,16 @@
 from datetime import timedelta
 
 from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.param_functions import Security
 from fastapi.security.oauth2 import OAuth2PasswordRequestForm
+from pydantic.env_settings import BaseSettings
 
+from app.config import settings
 from app.users.crud import create_user
 from app.users.schemas import BasicUserCreate, UserCreate, UserPublic
 
 from .schemas import Token
-from .utils import authenticate_user, create_access_token
+from .utils import authenticate_user, create_access_token, get_current_user
 
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
@@ -46,3 +49,14 @@ def register_basic_user(user: BasicUserCreate):
 
     real_user = UserCreate(**user.dict(), is_admin=False)
     return create_user(real_user)
+
+
+@router.get(
+    "/settings",
+    dependencies=[Security(get_current_user, scopes=["admin"])],
+    response_model=BaseSettings,
+)
+def get_settings():
+    """Returns the current api settings. Requires admin."""
+
+    return settings
