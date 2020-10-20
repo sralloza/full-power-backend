158a5d11ec102485dc70e813cd863fe37d866e9e
sralloza <sralloza@gmail.com>
2020-10-20 11:07:51 +0200

    feat: fix environment variables for dialogflow

--- app/config.py
+++ app/config.py
@@ -1,5 +1,6 @@
 """Internal settings of the API."""
 
+import os
 from pathlib import Path
 
 from pydantic import BaseSettings
@@ -8,11 +9,24 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     """Internal settings of the API"""
 
-    sqlalchemy_database_url: str
+    dialogflow_project_id: str
+    google_application_credentials: str
     production: bool = False
+    sqlalchemy_database_url: str
 
     class Config:
         env_file = Path(__file__).parent.with_name(".env").as_posix()
+        env_file_encoding = "utf-8"
+        fields = {
+            "dialogflow_project_id": {"env": "DIALOGFLOW_PROJECT_ID"},
+            "google_application_credentials": {"env": "GOOGLE_APPLICATION_CREDENTIALS"},
+        }
+
+    def set_environment(self):
+        os.environ[
+            "GOOGLE_APPLICATION_CREDENTIALS"
+        ] = self.google_application_credentials
 
 
 settings = Settings()
+settings.set_environment()
