52e9e35cef3755952ef51b4fdb042188990900b0
sralloza <sralloza@gmail.com>
2020-10-21 11:06:38 +0200

    feat: add settings LOG_PATH, MAX_LOGS and LOGGING_LEVEL

--- app/config.py
+++ app/config.py
@@ -1,9 +1,22 @@
 """Internal settings of the API."""
 
+import logging
 import os
+from enum import Enum
 from pathlib import Path
 
 from pydantic import BaseSettings, Field
+
+
+class ValidLoggingLevel(Enum):
+    DEBUG = "DEBUG"
+    INFO = "INFO"
+    WARNING = "WARNING"
+    ERROR = "ERROR"
+    CRITICAL = "CRITICAL"
+
+    def as_python_logging(self):
+        return logging._nameToLevel[self.value]
 
 
 class Settings(BaseSettings):
@@ -15,6 +28,11 @@ class Settings(BaseSettings):
     )
     production: bool = Field(False, env="PRODUCTION")
     sqlalchemy_database_url: str = Field(..., env="SQLALCHEMY_DATABASE_URL")
+    log_path: str = Field(..., env="LOG_PATH")
+    max_logs: int = Field(0, env="MAX_LOGS")
+    logging_level: ValidLoggingLevel = Field(
+        ValidLoggingLevel.INFO, env="LOGGING_LEVEL"
+    )
 
     class Config:
         env_file = Path(__file__).parent.with_name(".env").as_posix()
