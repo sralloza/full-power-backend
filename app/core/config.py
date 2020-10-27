de1c3dc2adcdad8fd4272bb8e47391a3acd64a47
sralloza <sralloza@gmail.com>
2020-10-27 23:01:47 +0100

    refactor: remove explicit env declaration from settings

--- app/core/config.py
+++ app/core/config.py
@@ -2,10 +2,11 @@
 
 import logging
 import os
+import secrets
 from enum import Enum
 from pathlib import Path
 
-from pydantic import BaseSettings, Field, validator
+from pydantic import BaseSettings
 
 
 class ValidLoggingLevel(Enum):
@@ -22,26 +23,19 @@ class ValidLoggingLevel(Enum):
 class Settings(BaseSettings):
     """Internal settings of the API"""
 
-    dialogflow_project_id: str = Field(..., env="DIALOGFLOW_PROJECT_ID")
-    google_application_credentials: str = Field(
-        ..., env="GOOGLE_APPLICATION_CREDENTIALS"
-    )
-    log_path: str = Field(..., env="LOG_PATH")
-    logging_level: ValidLoggingLevel = Field(
-        ValidLoggingLevel.INFO, env="LOGGING_LEVEL"
-    )
-    max_logs: int = Field(0, env="MAX_LOGS")
-    production: bool = Field(False, env="PRODUCTION")
-    server_secret: str = Field(..., env="SECRET")
-    sqlalchemy_database_url: str = Field(..., env="SQLALCHEMY_DATABASE_URL")
-    token_expire_minutes: int = Field(30, env="TOKEN_EXPIRE_MINUTES")
+    dialogflow_project_id: str
     encryption_algorithm: str = "HS256"
+    google_application_credentials: str
+    log_path: str
+    logging_level: ValidLoggingLevel = ValidLoggingLevel.INFO
+    max_logs: int = 0
+    production: bool = False
+    server_secret: str = secrets.token_urlsafe(32)
+    sqlalchemy_database_url: str
+    token_expire_minutes: int = 30
 
-    @validator("server_secret")
-    def validate_secret(cls, value):
-        if len(value) != 64:
-            raise ValueError("SECRET must contain 64 characters")
-        return value
+    first_superuser_password: str
+    first_superuser: str
 
     class Config:
         env_file = Path(__file__).parent.parent.with_name(".env").as_posix()
@@ -51,6 +45,8 @@ class Settings(BaseSettings):
         os.environ[
             "GOOGLE_APPLICATION_CREDENTIALS"
         ] = self.google_application_credentials
+
+    username_test_user: str = "the_Test"
 
 
 settings = Settings()
