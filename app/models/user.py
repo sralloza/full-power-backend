ac3436b25bf4cbff4472a05484f3a20749d9fcde
sralloza <sralloza@gmail.com>
2020-10-27 12:29:27 +0100

    refactor: move models to app.models

--- app/models/user.py
+++ app/models/user.py
@@ -2,9 +2,8 @@
 
 from sqlalchemy import Boolean, Column, Integer, String
 from sqlalchemy.orm import relationship
-from sqlalchemy.sql.schema import ForeignKey
 
-from . import Base
+from app.db.base_class import Base
 
 # pylint: disable=too-few-public-methods
 
@@ -28,16 +27,3 @@ class User(Base):
         if self.is_admin:
             return ["admin", "basic"]
         return ["basic"]
-
-
-class Conversation(Base):
-    """Represents a conversation between a user and the health bot."""
-
-    __tablename__ = "conversations"
-
-    id = Column(Integer, primary_key=True, index=True)
-    user_msg = Column(String(200), index=True, nullable=False)
-    bot_msg = Column(String(200), index=True, nullable=False)
-    intent = Column(String(50), index=True)
-    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
-    user = relationship("User", back_populates="conversations")
