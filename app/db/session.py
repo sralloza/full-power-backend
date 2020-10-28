fa358f946398938eae1c80303a8efbb4f2a6f07a
sralloza <sralloza@gmail.com>
2020-10-28 11:42:20 +0100

    fix: support sqlite databases (designed for testing)

--- app/db/session.py
+++ app/db/session.py
@@ -5,5 +5,12 @@ from sqlalchemy.orm import sessionmaker
 
 from app.core.config import settings
 
-engine = create_engine(settings.sqlalchemy_database_url, pool_pre_ping=True)
+if "sqlite" in settings.sqlalchemy_database_url:
+    connect_args = {"check_same_thread": False}
+else:
+    connect_args = {}
+
+engine = create_engine(
+    settings.sqlalchemy_database_url, pool_pre_ping=True, connect_args=connect_args
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
