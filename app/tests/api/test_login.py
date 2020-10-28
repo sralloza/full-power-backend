9130602ff1713740fd8cd0fa68efd79e9a9e4180
sralloza <sralloza@gmail.com>
2020-10-28 19:18:01 +0100

    test: finish covering login routes

--- app/tests/api/test_login.py
+++ app/tests/api/test_login.py
@@ -1,46 +1,85 @@
+import time
 from typing import Dict
 
 from fastapi.testclient import TestClient
+from sqlalchemy.orm.session import Session
 
+from app import crud
 from app.core.config import settings
+from app.models.user import User
+from app.schemas.token import Token
+from app.schemas.user import UserCreateAdmin
+from app.tests.conftest import client
+from app.tests.utils.utils import random_lower_string
 
 
-def test_get_access_token(client: TestClient) -> None:
+def test_get_access_token(client: TestClient):
     login_data = {
         "username": settings.first_superuser,
         "password": settings.first_superuser_password,
     }
-    r = client.post("/login", data=login_data)
-    tokens = r.json()
-    assert r.status_code == 200
+    response = client.post("/login", data=login_data)
+    tokens = response.json()
+    assert response.status_code == 200
     assert "access_token" in tokens
     assert tokens["access_token"]
 
 
 def test_use_access_token(client: TestClient, superuser_token_headers: Dict[str, str]):
-    r = client.get("/me", headers=superuser_token_headers)
-    result = r.json()
-    assert r.status_code == 200
+    response = client.get("/me", headers=superuser_token_headers)
+    result = response.json()
+    assert response.status_code == 200
     assert "username" in result
 
 
-def test_get_users_superuser_me(
-    client: TestClient, superuser_token_headers: Dict[str, str]
-):
-    r = client.get("/me", headers=superuser_token_headers)
-    current_user = r.json()
-    assert current_user
-    assert current_user["id"]
-    assert current_user["is_admin"]
-    assert current_user["username"] == settings.first_superuser
+def test_login_success(client: TestClient, db: Session):
+    username = random_lower_string()
+    password = random_lower_string()
+    user_in = UserCreateAdmin(username=username, password=password, is_admin=False)
+    crud.user.create(db, obj_in=user_in)
+
+    response = client.post("/login", data={"username": username, "password": password})
+    assert response.status_code == 200
+    token = Token(**response.json())
+    assert token
+    assert token.token_type == "bearer"
 
 
-def test_get_users_normal_user_me(
-    client: TestClient, normal_user_token_headers: Dict[str, str]
-):
-    r = client.get("/me", headers=normal_user_token_headers)
-    current_user = r.json()
-    assert current_user
-    assert current_user["id"]
-    assert current_user["is_admin"] is False
-    assert current_user["username"] == settings.username_test_user
+def test_login_error(client: TestClient):
+    username = random_lower_string()
+    password = random_lower_string()
+    response = client.post("/login", data={"username": username, "password": password})
+
+    error = response.json()
+    assert response.status_code == 401
+    assert error["detail"] == "Incorrect username or password"
+
+
+def test_register_basic_user(client: TestClient, db: Session):
+    username = random_lower_string()
+    password = random_lower_string()
+    response = client.post(
+        "/register", json={"username": username, "password": password}
+    )
+
+    assert response.status_code == 200
+    user= User(**response.json())
+    assert user
+
+    db_user = crud.user.get_by_username(db, username=username)
+    assert db_user
+    assert db_user.username == username
+    assert user.username == username
+
+
+def test_refresh_token(client: TestClient, normal_user_token_headers: dict):
+    token1 = normal_user_token_headers["Authorization"].replace("Bearer ", "")
+
+    time.sleep(1)
+    refresh_response = client.post("/refresh", headers=normal_user_token_headers)
+    assert refresh_response.status_code == 200
+    token2 = refresh_response.json()["access_token"]
+    assert token1 != token2
+
+    confirm_response = client.get("/me", headers={"Authorization": f"Bearer {token2}"})
+    assert confirm_response.status_code == 200
