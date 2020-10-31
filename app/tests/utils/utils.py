bd38fc5d77904924c53b9d137342e7796318a640
sralloza <sralloza@gmail.com>
2020-10-31 11:45:54 +0100

    test: remove testing utill random_email

--- app/tests/utils/utils.py
+++ app/tests/utils/utils.py
@@ -11,10 +11,6 @@ def random_lower_string() -> str:
     return "".join(random.choices(string.ascii_lowercase, k=32))
 
 
-def random_email() -> str:
-    return f"{random_lower_string()}@{random_lower_string()}.com"
-
-
 def random_int() -> int:
     return int("".join(random.choices(string.digits, k=10)))
 
