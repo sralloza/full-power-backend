7dc9a597de4f7c906e4ac6d953d03dd13c92878e
sralloza <sralloza@gmail.com>
2020-11-11 14:41:39 +0100

    feat: add timestamp column to Conversation model

--- app/models/conversation.py
+++ app/models/conversation.py
@@ -1,8 +1,9 @@
 """Database models."""
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, func
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.schema import ForeignKey
+from sqlalchemy.sql.sqltypes import DateTime
 
 from app.db.base_class import Base
 
@@ -18,5 +19,6 @@ class Conversation(Base):
     user_msg = Column(String(200), index=True, nullable=False)
     bot_msg = Column(String(200), index=True, nullable=False)
     intent = Column(String(50), index=True)
+    timestamp = Column(DateTime, server_default=func.now())
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     user = relationship("User", back_populates="conversations")
