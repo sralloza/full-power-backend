8f4cad28c5d85909ff4846aba80b9009c9f85bc4
sralloza <sralloza@gmail.com>
2020-11-23 10:03:21 +0100

    feat: add crud.health_data.get_user

--- app/crud/crud_health_data.py
+++ app/crud/crud_health_data.py
@@ -1,5 +1,7 @@
 import logging
 
+from sqlalchemy.orm.session import Session
+
 from app.models.health_data import HealthData
 from app.schemas.health_data import HealthDataCreate, HealthDataUpdate
 
@@ -9,7 +11,14 @@ logger = logging.getLogger(__name__)
 
 
 class CRUDHealthData(CRUDBase[HealthData, HealthDataCreate, HealthDataUpdate]):
-    pass
+    def get_user(self, db: Session, *, user_id: int, skip: int = 0, limit: int = 100):
+        return (
+            db.query(self.model)
+            .filter_by(user_id=user_id)
+            .offset(skip)
+            .limit(limit)
+            .all()
+        )
 
 
 health_data = CRUDHealthData(HealthData)
