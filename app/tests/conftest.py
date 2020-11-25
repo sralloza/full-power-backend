fc827e3d1c1b83da5087d9ec997f936b35a245b7
sralloza <sralloza@gmail.com>
2020-11-25 10:27:11 +0100

    test: refactor test utils

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -3,20 +3,22 @@ from typing import Dict, Generator
 
 import pytest
 from fastapi.testclient import TestClient
-from sqlalchemy.orm import Session
 
 from app import app
-from app.core.config import settings
 from app.db.init_db import init_db
 from app.db.session import SessionLocal
-from app.tests.utils.user import authentication_token_from_username
-from app.tests.utils.utils import get_superuser_token_headers
+from app.tests.utils.user import (
+    create_test_user,
+    get_normal_user_token_headers,
+    get_superuser_token_headers,
+)
 
 
 @pytest.fixture(scope="session")
 def db() -> Generator:
     db = SessionLocal()
     init_db(db)
+    create_test_user(db)
     yield db
     db.close()
     os.remove("testing-database.db")
@@ -34,7 +36,5 @@ def superuser_token_headers(client: TestClient) -> Dict[str, str]:
 
 
 @pytest.fixture(scope="module")
-def normal_user_token_headers(client: TestClient, db: Session) -> Dict[str, str]:
-    return authentication_token_from_username(
-        client=client, username=settings.username_test_user, db=db
-    )
+def normal_user_token_headers(client: TestClient) -> Dict[str, str]:
+    return get_normal_user_token_headers(client)
