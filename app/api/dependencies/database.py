edbddc441097499ddbde058c9d342fb7d6bca1e5
sralloza <sralloza@gmail.com>
2020-12-05 23:36:37 +0100

    docs: comment dependencies

--- app/api/dependencies/database.py
+++ app/api/dependencies/database.py
@@ -1,7 +1,10 @@
+"""Database dependencies."""
+
 from app.db.session import SessionLocal, engine
 
 
 async def get_db():
+    """Creates a local database session."""
     db = SessionLocal()
     try:
         yield db
