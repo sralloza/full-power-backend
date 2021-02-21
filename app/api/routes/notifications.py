bcc33f95dfa8f2646f892c912664f472f3cff00c
sralloza <sralloza@gmail.com>
2021-02-21 12:54:34 +0100

    feat: update notifications endpoint to return the question_id

--- app/api/routes/notifications.py
+++ app/api/routes/notifications.py
@@ -2,6 +2,7 @@ import random
 
 from fastapi import APIRouter
 
+from app.schemas.notification import QuestionNotification
 from app.utils.translate import i18n
 
 router = APIRouter(prefix="/notifications-content", tags=["notifications"])
@@ -9,23 +10,28 @@ router = APIRouter(prefix="/notifications-content", tags=["notifications"])
 
 @router.get(
     "/second-survey/{problem}",
-    response_model=str,
+    response_model=QuestionNotification,
     summary="Second survey notifications",
 )
 def get_second_survey_notifications(problem: str, lang: str):
+    # TODO: test that problem exists
+    # TODO: test that lang exists
     i18n.set("locale", lang)
     nchoices = int(i18n.t(f"notifications.{problem}.length"))
-    messages = [
-        i18n.t(f"notifications.{problem}.notif{i}") for i in range(1, nchoices + 1)
-    ]
-    return random.choice(messages)
+    choice = random.randint(1, nchoices)
+    return QuestionNotification(
+        content=i18n.t(f"notifications.{problem}.notif{choice}"),
+        id=f"{problem}.{choice}",
+    )
 
 
-@router.get("/generic", response_model=str, summary="Generic notifications")
+@router.get("/generic", response_model=QuestionNotification, summary="Generic notifications")
 def get_generic_notifications(lang: str):
+    # TODO: test that lang exists
     i18n.set("locale", lang)
     nchoices = int(i18n.t(f"notifications.generic.length"))
-    messages = [
-        i18n.t(f"notifications.generic.notif{i}") for i in range(1, nchoices + 1)
-    ]
-    return random.choice(messages)
+    choice = random.randint(1, nchoices)
+    return QuestionNotification(
+        content=i18n.t(f"notifications.generic.notif{choice}"),
+        id=f"generic.{choice}",
+    )
