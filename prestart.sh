98df3a3d9857208e9f5f0d5f21407b38e0b2f282
Diego Alloza González <sralloza@gmail.com>
2021-03-20 18:59:16 +0100

    chore: improve prestart script

--- prestart.sh
+++ prestart.sh
@@ -1,4 +1,14 @@
-./wait-for-it.sh -t 30 database:3306
+export SQLALCHEMY_DATABASE_URL="mysql+pymysql://root:${MYSQL_ROOT_PASSWORD}@database:${MYSQL_PORT}/backend"
+echo "backend's database: '${SQLALCHEMY_DATABASE_URL}'"
+
+if [ -z "$MYSQL_PORT" ]; then
+    echo "Must provide MYSQL_PORT in environment" 1>&2
+    exit 2;
+fi
+
+python -c "from app.core.config import settings;print(repr(settings))"
+
+./wait-for-it.sh -t 30 "database:${MYSQL_PORT}"
 
 # Run migrations
 alembic upgrade head
