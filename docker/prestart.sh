93513db519709cd2026200ceb3041bcec0f968e2
Diego Alloza González <sralloza@gmail.com>
2021-03-23 21:38:16 +0100

    chore: set up arm docker images

--- docker/prestart.sh
+++ docker/prestart.sh
@@ -1,16 +1,25 @@
-export MYSQL_PORT=3306
 export SQLALCHEMY_DATABASE_URL="mysql+pymysql://root:${MYSQL_ROOT_PASSWORD}@database:${MYSQL_PORT}/backend"
+export PATH=$(pwd):$PATH
+export PYTHONPATH=$(pwd)
+export APP_MODULE=app:app
+
 echo "backend's database: '${SQLALCHEMY_DATABASE_URL}'"
+echo "path: '${PATH}'"
+echo "pythonpath: '${PYTHONPATH}'"
+echo "app_module: '${APP_MODULE}'"
 
 if [ -z "$MYSQL_PORT" ]; then
     echo "Must provide MYSQL_PORT in environment" 1>&2
     exit 2;
 fi
 
-python -c "from app.core.config import settings;print(repr(settings))"
-
+# Wait for database up
 ./wait-for-it.sh -t 30 "database:${MYSQL_PORT}"
 
+# Show settings for debugging purposes
+python -c "from app.core.config import settings;print(repr(settings))"
+
+# Create the database
 python ./scripts/ensure-database.py
 
 # Run migrations
